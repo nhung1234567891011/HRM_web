02db60940b7bcf5e2f532aec5b0ff2c0a3281eef
tuancter <tuanphuong130204@gmail.com>
2026-03-22 11:39:32 +0700

    update

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -6,14 +6,16 @@ import { UtilityModule } from 'src/app/core/modules/utility/utility.module';
 import { RecoverPasswordComponent } from './recover-password/recover-password.component';
 import { SetPasswordComponent } from './set-password/set-password.component';
 import { ToastModule } from 'primeng/toast';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
-    declarations:[LoginComponent,RecoverPasswordComponent,SetPasswordComponent],
+    declarations: [LoginComponent, RecoverPasswordComponent, SetPasswordComponent],
     imports: [
         CommonModule,
         AuthRoutingModule,
         UtilityModule,
-        ToastModule
+        ToastModule,
+        ButtonModule
     ]
 })
 export class AuthModule { }
