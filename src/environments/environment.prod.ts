ebb80c3ee80a03232f62d918e4701be827311563
tuancter <tuanphuong130204@gmail.com>
2026-04-09 01:45:14 +0700

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
     production: true,
     baseFeUrl: 'https://hrm.smomedia.vn',
-    baseApiUrl: 'api-hrm-gzhfhkcra3bbd7bc.southeastasia-01.azurewebsites.net/api',
+    // Thêm https:// vào dòng dưới đây
+    baseApiUrl: 'https://api-hrm-gzhfhkcra3bbd7bc.southeastasia-01.azurewebsites.net/api', 
     baseApiImageUrl: 'https://api-hrm-gzhfhkcra3bbd7bc.southeastasia-01.azurewebsites.net',
     baseSignLRUrl: 'https://api-hrm-gzhfhkcra3bbd7bc.southeastasia-01.azurewebsites.net',
     baseApiUploadFile: 'https://api-hrm-gzhfhkcra3bbd7bc.southeastasia-01.azurewebsites.net/api/file/upload',
