01830069230be3ec39b6dcff059bb558162fc66a
tuancter <tuanphuong130204@gmail.com>
2026-04-09 01:36:36 +0700

    update

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: true,
+    baseFeUrl: 'https://hrm.smomedia.vn',
+    baseApiUrl: 'api-hrm-gzhfhkcra3bbd7bc.southeastasia-01.azurewebsites.net/api',
+    baseApiImageUrl: 'https://api-hrm-gzhfhkcra3bbd7bc.southeastasia-01.azurewebsites.net',
+    baseSignLRUrl: 'https://api-hrm-gzhfhkcra3bbd7bc.southeastasia-01.azurewebsites.net',
+    baseApiUploadFile: 'https://api-hrm-gzhfhkcra3bbd7bc.southeastasia-01.azurewebsites.net/api/file/upload',
+    baseApiDeleteFile: 'https://api-hrm-gzhfhkcra3bbd7bc.southeastasia-01.azurewebsites.net/api/file/delete',
+};
